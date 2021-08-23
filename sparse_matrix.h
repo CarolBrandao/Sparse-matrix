22d23907ddfb8a7422abba6442a018e804c0d838
carolbrandao <bcarol744@gmail.com>
2021-08-23 19:30:22 +0200

    feat: add header file

---
+++ sparse_matrix.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct TCell{
+    struct TCell *right, *bottom;
+    int line, column;
+    float value;
+}TCell;
+
+typedef struct{
+    int lineSize, columnSize; 
+    TCell *start, *endLine, *endColumn;
+}TMatrix;
+
+int Insert(TMatrix *mat, int line, int column, float value);
+void PrintMatrix(TMatrix *A);
+void DeleteMatriz(TMatrix *A);
+
+TMatrix *InitializeMatrix(int lines, int columns);
+TMatrix *ReadMatrix(TMatrix *A);
+TMatrix *SumMatrix(TMatrix *A, TMatrix *B);
+TMatrix *MultipliesMatrix(TMatrix *A, TMatrix *B);
+
+float returnCellValue(TMatrix *mat, int line, int column);
